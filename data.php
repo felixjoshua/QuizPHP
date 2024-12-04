48652f34d89868cbf163a4d41c1f6ce0fda580bc
Felix Joshua Paulus <130731253+felixjoshua@users.noreply.github.com>
2024-12-04 11:59:26 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,4 +1,3 @@
-<?php
 $quizData = [
     [
         'type' => 'multiple',
@@ -18,6 +17,87 @@ $quizData = [
         'question' => 'Pulau manakah yang terbesar di Indonesia?',
         'options' => ['Jawa', 'Sumatra', 'Kalimantan', 'Papua'],
         'correct' => 'Papua',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Siapa Presiden pertama Indonesia?',
+        'options' => ['Soeharto', 'Bacharuddin Jusuf Habibie', 'Ir. Soekarno', 'Susilo Bambang Yudhoyono'],
+        'correct' => 'Ir. Soekarno',
+        'points' => 5
+    ],
+    [
+        'type' => 'text',
+        'question' => 'Apa nama gunung tertinggi di Indonesia?',
+        'correct' => 'Carstensz Pyramid',
+        'points' => 10
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Apa nama mata uang Indonesia?',
+        'options' => ['Ringgit', 'Rupiah', 'Baht', 'Peso'],
+        'correct' => 'Rupiah',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Apa nama salah satu makanan khas Indonesia yang terbuat dari ketan?',
+        'options' => ['Rendang', 'Nasi Goreng', 'Lontong', 'Klepon'],
+        'correct' => 'Klepon',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Di kota manakah candi Borobudur berada?',
+        'options' => ['Yogyakarta', 'Solo', 'Semarang', 'Magelang'],
+        'correct' => 'Magelang',
+        'points' => 5
+    ],
+    [
+        'type' => 'text',
+        'question' => 'Siapa tokoh proklamator kemerdekaan Indonesia bersama Ir. Soekarno?',
+        'correct' => 'Mohammad Hatta',
+        'points' => 10
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Apa nama laut yang berada di sebelah utara Indonesia?',
+        'options' => ['Laut Cina Selatan', 'Laut Jawa', 'Laut Banda', 'Laut Sulu'],
+        'correct' => 'Laut Cina Selatan',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Apa nama ibu kota Jawa Timur?',
+        'options' => ['Surabaya', 'Malang', 'Batu', 'Jember'],
+        'correct' => 'Surabaya',
+        'points' => 5
+    ],
+    [
+        'type' => 'text',
+        'question' => 'Siapa penulis novel Laskar Pelangi?',
+        'correct' => 'Andrea Hirata',
+        'points' => 10
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Siapa yang menciptakan lagu “Bengawan Solo”?',
+        'options' => ['Ismail Marzuki', 'W.R. Supratman', 'Gesang', 'Gombloh'],
+        'correct' => 'Gesang',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Di manakah kota Bali berada?',
+        'options' => ['Sumatra', 'Sulawesi', 'Kalimantan', 'Nusa Tenggara'],
+        'correct' => 'Nusa Tenggara',
+        'points' => 5
+    ],
+    [
+        'type' => 'multiple',
+        'question' => 'Apa nama ibu kota negara Malaysia?',
+        'options' => ['Kuala Lumpur', 'Singapura', 'Bangkok', 'Hanoi'],
+        'correct' => 'Kuala Lumpur',
         'points' => 5
     ],
 ];
